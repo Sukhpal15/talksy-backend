93dea4643cbb1e4e78e1553fe8313f99d90d02f0
Sukhpal <sukhpal151515@gmail.com>
2025-12-23 10:58:48 +0530

    image limiting

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 app.set("trust proxy", 1);
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
